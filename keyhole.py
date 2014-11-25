c670a5a5c8bec6d2baef460281b4f2f20a62f036
Vincent Méthot <vincent.methot@gmail.com>
2014-11-25 18:35:52 -0500

    Acceleration of the interpolation

--- keyhole.py
+++ keyhole.py
@@ -3,7 +3,6 @@
 
 # Script to reconstruct keyhole data from a fid + procpar files
 
-
 print ''
 print '#########################################'
 print 'Importing libraries'
@@ -11,17 +10,18 @@ print '#########################################'
 print ''
 
 import numpy as np
-# import scipy as sp
-from scipy.interpolate import interp1d
 import nibabel as nib
-from struct import unpack
-from nmrglue.fileio.varian import read_procpar
-import datetime
-import sys
-import os
 from variantools import *
+import multiprocessing as mp
 
-path = sys.argv[1]
+# import sys
+# input_path = sys.argv[1]
+# module_path = sys.argv[0][:-10]
+# output = sys.argv[2]
+
+input_path = '/home/vincent/Maitrise/Data/metv_20141124_001/data/gems_ep04_01.fid'
+module_path = '/home/vincent/Programmation/Python/varian_tools'
+output_path = 'test.nii.gz'
 
 print ''
 print '#########################################'
@@ -29,8 +29,8 @@ print 'Extract fid data and procpar parameters'
 print '#########################################'
 print ''
 
-par = parseprocpar(path + '/procpar')
-data = loadfid(path + '/fid')
+par = parseprocpar(input_path + '/procpar')
+data = loadfid(input_path + '/fid')
 
 print ''
 print '#########################################'
@@ -39,7 +39,7 @@ print '#########################################'
 print ''
 
 # Get phase encode table from the scan parameters
-petable = read_petable(sys.argv[0][:-10] + '/' + par['petable'])
+petable = read_petable(module_path + '/' + par['petable'])
 # Count number of zero in petable to know number of frame per acquisition
 frames_per_acq = np.sum(petable == 0)
 time_frames = frames_per_acq * data.shape[0]
@@ -60,7 +60,7 @@ petable -= petable.min()
 kspace = np.empty([time_frames, ns, petable.max() + 1,
                    data.shape[-1]], dtype='complex64')
 kspace[...] = np.nan
- 
+
 # Optimization possibility (removing for loops)
 for b in range(par['arraydim']):
     for f in range(frames_per_acq):
@@ -84,31 +84,23 @@ def interp_keyhole(time_serie):
     acquired.
     Array dim: (time point, slice number, phase encode, freq. encode)
     """
+    # Position points to non-nan elements (which has been acquired)
     position = -np.isnan(abs(time_serie))
+    # For center of k-space, no keyhole interpolation required
+    if -np.any(np.isnan(time_serie)):
+        return time_serie
+
+    # Interpolation is linear, with numpy function 'interp'
     time_frames = time_serie.shape[0]
     k0 = time_serie[position]
     t0 = np.arange(time_frames)[position]
-
-    if -position[0]:
-        k0 = np.append(k0[0], k0)
-        t0 = np.append(0, t0)
-    if -position[-1]:
-        k0 = np.append(k0, k0[-1])
-        t0 = np.append(t0, time_frames - 1)
-    
     t1 = np.arange(time_serie.shape[0])
-    fr = interp1d(t0, np.real(k0),kind='linear')
-    fi = interp1d(t0, np.imag(k0),kind='linear')
-    k1 = fr(t1) + complex(0,1)*fi(t1)
+    fr = np.interp(t1, t0, np.real(k0), np.real(k0[0]), np.real(k0[-1]))
+    fi = np.interp(t1, t0, np.imag(k0), np.imag(k0[0]), np.imag(k0[-1]))
+    k1 = fr + fi*1j
     return k1
 
-modified_kspace = np.empty(kspace.shape, dtype=np.complex64)
-for i in np.ndindex(kspace.shape[1:]):
-    if -np.any(np.isnan(kspace[:, i[0], i[1], i[2]])):
-        modified_kspace[:, i[0], i[1], i[2]] = kspace[:, i[0], i[1], i[2]]
-    else:
-        modified_kspace[:, i[0], i[1], i[2]] = interp_keyhole(kspace[:, i[0], i[1], i[2]])
-        print 'Interpolating kspace data point:', 'slice', i[0], 'kpoint', i[1], i[2]
+kspace = np.apply_along_axis(interp_keyhole, 0, kspace)
 
 print ''
 print '#########################################'
@@ -117,7 +109,7 @@ print '#########################################'
 print ''
 
 for i in range(5):
-    modified_kspace -= modified_kspace.mean()
+    kspace -= kspace.mean()
 
 print ''
 print '#########################################'
@@ -125,39 +117,32 @@ print '2D Fourier Transform'
 print '#########################################'
 print ''
 
-image_tmp = np.abs((np.fft.fft2(modified_kspace)))
+image_tmp = np.abs((np.fft.fft2(kspace)))
 image_tmp = np.fft.fftshift(image_tmp, axes=(2,3))
 image_tmp = image_tmp.transpose([2,3,1,0])
 
-
-# Image x(phase), y(frequency), z(slice), t(time)
-
-# print ''
-# print '#########################################'
-# print 'Reorder slices'
-# print '#########################################'
-# print ''
-# 
-# image = np.empty(image_tmp.shape)
-# interleave_order = range(ns)
-# interleave_order = interleave_order[::2] + interleave_order[1::2]
-# for z in range(ns):
-    # image[:,:,interleave_order[z], :] = image_tmp[:,::-1, z, :]
-# 
-image = image_tmp
-
+# Image dimensions are now: x(phase), y(frequency), z(slice), t(time)
 
 print ''
 print '#########################################'
-print 'Saving image in "' + sys.argv[2] + '"'
+print 'Reorder slices'
 print '#########################################'
 print ''
 
+image = np.empty(image_tmp.shape)
+interleave_order = range(ns)
+interleave_order = interleave_order[::2] + interleave_order[1::2]
+for z in range(ns):
+    image[:,:,interleave_order[z], :] = image_tmp[:,::-1, z, :]
+
+print ''
+print '#########################################'
+print 'Saving image in', output_path
+print '#########################################'
+print ''
 
 affine = np.eye(4)
 dx, dy, dz = 0.250, 0.250, 1.000
 affine[np.eye(4) == 1] = [dx, dy, dz, 1]
 nifti = nib.Nifti1Image(image, affine)
-nib.save(nifti, sys.argv[2])
-
-
+nib.save(nifti, output_path)
