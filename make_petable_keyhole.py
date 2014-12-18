386cfe038fe76b27ebe6a78eb08af3e62c580e43
Vincent Méthot <vincent.methot@gmail.com>
2014-12-18 14:00:23 -0500

    Create customs PETables for keyhole

---
+++ make_petable_keyhole.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+########################################################################
+# Author: Vincent Méthot                                               #
+# Creation date: 2014/12/12                                            #
+# Make petable for keyhole acquisition                                 #
+########################################################################
+
+import numpy as np
+# import nibabel as nib
+# from struct import unpack
+# from nmrglue.fileio.varian import read_procpar
+# import datetime
+import sys
+# import os
+
+acquisition_per_frame = int(sys.argv[1])
+time_frames = int(sys.argv[2])
+resolution = int(sys.argv[3])
+
+# To acquire 60% of kspace
+half_kspace = False
+
+perange = range(-resolution/2, resolution/2)
+center = resolution / 4
+border = 3 * resolution / (4 * time_frames)
+
+pepoints = []
+for t in range(time_frames):
+    start = -resolution / 2 + t * border / 2
+    end = start + border / 2
+    if not(half_kspace):
+        pepoints += range(start, end)
+        pepoints += range(-resolution / 8, -resolution / (8*4))
+    pepoints += range(-resolution / (8*4), resolution / 8)
+    pepoints += range(-end, -start)
+
+print 'Nombre de points:', len(pepoints)
+
+string = str(pepoints)[1:-1]
+
+if half_kspace:
+    f = open('gems_half_' + str(acquisition_per_frame) + '_' + str(time_frames) + '_' + str(resolution), 'w')
+else:
+    f = open('gems_' + str(acquisition_per_frame) + '_' + str(time_frames) + '_' + str(resolution), 'w')
+    
+f.write('t1 =\n')
+f.write(string.replace(',', ''))
+f.close()
