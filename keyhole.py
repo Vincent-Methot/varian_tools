2ec4c75a768ebcfa369214ee66eba3931e3be346
Vincent Méthot <vincent.methot@gmail.com>
2015-01-05 16:19:10 -0500

    Return of keyhole

--- keyhole.py
+++ keyhole.py
@@ -13,7 +13,7 @@ par = vt.load_procpar(inpath + '/procpar')
 kspace = vt.reconstruct_keyhole(fid, par)
 image = vt.fourier_transform(kspace)
 
-# image = vt.reorder_interleave(image)
-image = image[:, ::-1, :, :]
+image = vt.reorder_interleave(image)
+# image = image[:, ::-1, :, :]
 
 vt.save_nifti(outpath, image, par)
