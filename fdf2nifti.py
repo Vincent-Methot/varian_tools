96bac2fe8d857d127d1ec29d5f397153eed4fbbf
Vincent Méthot <vincent.methot@gmail.com>
2015-04-29 14:22:42 -0400

    Interleaved correction

--- fdf2nifti.py
+++ fdf2nifti.py
@@ -12,7 +12,17 @@ outpath = sys.argv[2]
 data = vt.load_fdf(inpath)
 par = vt.load_procpar(inpath + '/procpar')
 
-data = data.transpose(2,1,0,3)
+
+
+# gems
+if par['layout'] == 'gems':
+	data = data.transpose(2,1,0,3)
+	data = data[::-1,::-1,:,:]
+# fsems
+if par['layout'] == 'fsems':
+	data = data.transpose(2,1,0,3)
+	data = data[::-1,::-1,:,:]
+
 vt.print_procpar(par)
 
 vt.save_nifti(outpath, data, par)
